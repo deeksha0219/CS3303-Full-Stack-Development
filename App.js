f14944999e6335bea2f3576405ee8a3cdc36595d
deeksha0219 <deekshabca24@rvu.edu.in>
2026-08-05 11:03:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src = "https://media.istockphoto.com/id/1166400008/photo/character-cartoon-girl-on-a-white-background-illustration-for-advertising.jpg?b=1&s=612x612&w=0&k=20&c=XU5TpQ-aXS1dX162_kI9vFRIPrDSFfGK7Q6GWYPLpv4="width="400" height="300"/>
+     
+      Deeksha
+    </div>
+  );
+}
+export default App;
